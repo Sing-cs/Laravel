51d513e021327a357f456156de0fd62e6a312437
Sing-cs <38367313+Sing-cs@users.noreply.github.com>
2020-07-18 14:10:59 +0800

    Update title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Installed
+                    Laravel
                 </div>
 
                 <div class="links">
